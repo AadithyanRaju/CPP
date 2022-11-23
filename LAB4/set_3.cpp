5e67505912614a01de3e47d229ec89c6bfb47e98
AadithyanRaju <aadithyan75@gmail.com>
2022-11-23 22:30:32 +0530

    lab4

--- LAB4/set_3.cpp
+++ LAB4/set_3.cpp
@@ -1,10 +1,31 @@
-#include <iostream>
 #include <set>
+#include <iostream>
 using namespace std;
-int main(int argc, char const *argv[])
+int main()
 {
-    set<int,int>s;
-    s.insert(1,9);
-    cout << *s->first<<*s->second;
+    set<pair <int,int> > s;
+    int count=0,cnt=0;
+    s.insert(make_pair(1,9));
+    s.insert(make_pair(10,20));
+    s.insert(make_pair(21,40));
+    s.insert(make_pair(41,60));
+    s.insert(make_pair(61,80));
+    s.insert(make_pair(81,100));
+    int a[]={1,30,25,60,75,41};
+    int b[]={0,0,0,0,0,0};
+    for (auto const &i : s)
+    {
+        for(int j=0;j<6;j++)if(i.first<=a[j] and a[j]<=i.second)cnt+=1;
+        b[count]=cnt;
+        count+=1;
+        cnt=0;
+    }
+    count =0;
+    for (auto const &i : s)
+    {
+        cout<<"Number of peoples in group "
+            <<i.first<<"-"<<i.second<<" is "<<b[count]<<endl;
+        count++;
+    }
     return 0;
 }
