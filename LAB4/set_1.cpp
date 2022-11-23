5c14c2d9a0c3d9618d6968dac9580febd1510cb3
Aadithyan <aadithyan75@gmail.com>
2022-11-23 19:22:03 +0530

    lab4

---
+++ LAB4/set_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main()
+{
+    set<int>s;
+    set<int>::iterator i;
+    set<int>::iterator f;
+    int j,x,y,z;
+    i=s.begin();
+    cout<<"Enter the number of iterations : ";cin>>z;
+    for(int k=0;k<z;k++){
+        cout<<"1. insert\n2. search \nChoice : ";
+        cin>>x;
+        if(x==1)
+        {
+            cout<<"Enter number: ";cin>>y;
+            s.insert(y);
+            cout<<"NUMBER INSERTED SUCCEFULLY"<<endl;
+        }else{
+            cout<<"Enter number to search : ";
+            cin>>y;
+            if(s.find(y)!=s.end())cout<<"FOUND"<<endl;
+            else cout<<"NOT FOUND"<<endl;
+        }
+    }
+    return 0;
+}
